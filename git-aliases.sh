e7a046bd5d5bd283ca1b53d75d80f73120ed142f
Eduardo Quintana <eduardo.quintana@derivco.co.uk>
2025-05-21 10:45:08 +0200

    Reorganize aliases

--- git-aliases.sh
+++ git-aliases.sh
@@ -1,29 +1,22 @@
-# === Git Alias Index ===
-# Clone:         gcl
-# Add:           ga, gas, gass, gagr
-# Commit:        gc, gca, gcam, gcne
-# Branch:        gb, gbv, gba, gbr, gbd, gbdf, gbm, gco, gcot, gcob, gcobgr
-# Merge:         gm, gma, gmc, gms
-# Fetch:         gf, gfs
-# Pull:          gpl, gpr
-# Push:          gpo, gpof
-# Rebase:        gr, gri, gra, grc
-# Stash:         gsu, gsm, gsd, gsp, gsl, gsc
-# Log:           glog, glogm, glh, gluh, glm
-# Show:          gbl, gd, gds, ggr, gsh, gshno
-# Reset:         grm, grhh, grh, grch
-# Diff:          gd, gds, gdgr
-# Status:        gs, gss
-# Reflog:        gref
-# File Checkout: gcofgr
-# PR:            pr
-# GitHub:        gh
-# Utils:         gcurrent, gdefault, gcgl, gcge
-
+# ============================ Clone ============================
 alias gcl="git clone" # Clone a repository
+
+# ============================ Add ============================
 alias ga="git add" # Add files to the staging area
 alias gas="git add -A && git status -s" # Add all changes to the staging area and show a short status
-alias gbl="git blame --color-by-age --color-lines" # Show blame information with color-by-age and color-lines
+
+# Add a file based on a partial name match from modified files
+function gagr() {
+  __git_match_and_execute "gagr" "$1" "git add"
+}
+
+# ============================ Commit ============================
+alias gc="git commit -m" # Commit with a message
+alias gca="git commit --amend --no-edit" # Amend the last commit without changing the message
+alias gcam="git commit --amend -m" # Amend the last commit and edit the message
+alias gcne="git commit --no-edit" # Commit without editing the message
+
+# ============================ Branch ============================
 alias gb="git branch" # List branches
 alias gbv="git branch -vv" # List branches with verbose information
 alias gba="git branch -a" # List all branches (local and remote)
@@ -34,88 +27,6 @@ alias gbm="git branch -m" # Rename the current branch
 alias gco="git checkout" # Switch branches
 alias gcot="git checkout --track" # Switch to a remote branch and track it
 alias gcob="git checkout -b" # Create and switch to a new branch
-alias gcfd="git clean -fd" # Remove untracked files and directories
-alias gc="git commit -m" # Commit with a message
-alias gca="git commit --amend --no-edit" # Amend the last commit without changing the message
-alias gcam="git commit --amend -m" # Amend the last commit and edit the message
-alias gcne="git commit --no-edit" # Commit without editing the message
-alias gd="git diff" # Show changes between commits, branches, or the working directory
-alias gds="git diff --staged" # Show changes in the staging area
-alias gf="git fetch" # Fetch changes from the remote
-alias gfs="git fetch && git status" # Fetch changes and show the status
-alias ggr="git grep --no-index -i -I --exclude-standard --heading --line-number" # Search for a string in the repository
-alias glog="git log --graph --pretty=format:'%C(yellow)%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit" # Show a graphical log with commit details
-alias glogm="glog --author='$(git config --get user.email)'" # Show a graphical log with commits by the current user
-alias glh="glog HEAD.." # Show commits in other branches not yet merged into HEAD
-alias gluh="glog @{u}..HEAD" # Show commits not pushed to the upstream branch
-alias gm="git merge --no-edit" # Merge branches without opening an editor
-alias gma="git merge --abort" # Abort a merge
-alias gmc="git merge --continue" # Continue a merge after resolving conflicts
-alias gms="git merge --squash" # Squash commits during a merge
-alias gpl="git pull" # Pull changes from the remote
-alias gpr="git pull -r" # Pull changes and rebase
-alias gpo="git push -u origin HEAD" # Push the current branch to the remote and set upstream
-alias gpof="git push -u origin HEAD --force" # Force push the current branch to the remote
-alias gr="git rebase" # Rebase the current branch
-alias gri="git rebase -i" # Start an interactive rebase
-alias grc="git rebase --continue" # Continue a rebase after resolving conflicts
-alias gra="git rebase --abort" # Abort a rebase
-alias gref="git reflog" # Show the reflog
-alias grm="git reset --mixed" # Reset index but keep changes in the working directory (mixed mode)
-alias grhh="git reset HEAD --hard" # Discards all uncommitted changes (hard reset).
-alias gsh="git show" # Show details of a commit
-alias gshno="git show --name-only" # Show names of files changed in a commit
-alias gs="git status" # Show the status of the working directory
-alias gss="git status -s" # Show a short status of the working directory
-alias gsu="git stash -u" # Stash untracked changes
-alias gsm="git stash -u -m" # Stash untracked changes with a message
-alias gsd="git stash drop" # Drop a stash
-alias gsp="git stash pop" # Apply the most recent stash
-alias gsl="git stash list" # List all stashes
-alias gsc="git stash clear" # Clear all stashes
-alias gdefault="git symbolic-ref refs/remotes/origin/HEAD | cut -d'/' -f4" # Get the default branch name
-alias gcurrent="git symbolic-ref --short HEAD" # Get the current branch name
-alias gcgl="git config --global --list" # List the current global Git configuration
-alias gcge="git config --global --edit" # Opens the global Git configuration file
-
-# Reset the current branch to n commits before HEAD
-function grh() {
-  if [ -z "$1" ]; then
-    echo "Usage: grh <number-of-commits>"
-    return 1
-  fi
-  git reset HEAD~$1
-}
-
-# Reset the current branch to the specified commit and apply --hard
-function grch() {
-  if [ -z "$1" ]; then
-    echo "Usage: grch <commit-hash>"
-    return 1
-  fi
-  git reset "$1" --hard
-}
- 
-# Display a limited number of recent Git log entries (default: all)
-function gl() {
-    local count=${1:--1}
-    glog -n $count
-}
- 
-# Display a limited number of recent Git log entries (default: all) by the author logged in
-function glm() {
-    local count=${1:--1}
-    glogm -n $count
-}
-
-# Show changes of a specific stash
-function gssh() {
-  if [ -z "$1" ]; then
-    echo "Usage: gssh <stash-index>"
-    return 1
-  fi
-  git stash show -p "stash@{$1}"
-}
 
 # Check out a branch based on a partial name match.
 function gcobgr() {
@@ -143,21 +54,116 @@ function gcobgr() {
   fi
 }
 
-# Check out a file based on a partial name match from modified files
-function gcofgr() {
-  __git_match_and_execute "gcofgr" "$1" "git checkout"
+# ============================ Merge ============================
+alias gm="git merge --no-edit" # Merge branches without opening an editor
+alias gma="git merge --abort" # Abort a merge
+alias gmc="git merge --continue" # Continue a merge after resolving conflicts
+alias gms="git merge --squash" # Squash commits during a merge
+
+# ============================ Fetch ============================
+alias gf="git fetch" # Fetch changes from the remote
+alias gfs="git fetch && git status" # Fetch changes and show the status
+
+# ============================ Pull ============================
+alias gpl="git pull" # Pull changes from the remote
+alias gpr="git pull -r" # Pull changes and rebase
+
+# ============================ Push ============================
+alias gpo="git push -u origin HEAD" # Push the current branch to the remote and set upstream
+alias gpof="git push -u origin HEAD --force" # Force push the current branch to the remote
+
+# ============================ Rebase ============================
+alias gr="git rebase" # Rebase the current branch
+alias gri="git rebase -i" # Start an interactive rebase
+alias gra="git rebase --abort" # Abort a rebase
+alias grc="git rebase --continue" # Continue a rebase after resolving conflicts
+
+# ============================ Stash ============================
+alias gsu="git stash -u" # Stash untracked changes
+alias gsm="git stash -u -m" # Stash untracked changes with a message
+alias gsd="git stash drop" # Drop a stash
+alias gsp="git stash pop" # Apply the most recent stash
+alias gsl="git stash list" # List all stashes
+alias gsc="git stash clear" # Clear all stashes
+
+# Show changes of a specific stash
+function gssh() {
+  if [ -z "$1" ]; then
+    echo "Usage: gssh <stash-index>"
+    return 1
+  fi
+  git stash show -p "stash@{$1}"
 }
 
-# Add a file based on a partial name match from modified files
-function gagr() {
-  __git_match_and_execute "gagr" "$1" "git add"
+# ============================ Log ============================
+alias glog="git log --graph --pretty=format:'%C(yellow)%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit" # Show a graphical log with commit details
+alias glogm="glog --author='$(git config --get user.email)'" # Show a graphical log with commits by the current user
+alias glh="glog HEAD.." # Show commits in other branches not yet merged into HEAD
+alias gluh="glog @{u}..HEAD" # Show commits not pushed to the upstream branch
+
+# Display a limited number of recent Git log entries (default: all)
+function gl() {
+    local count=${1:--1}
+    glog -n $count
 }
+
+# Display a limited number of recent Git log entries (default: all) by the author logged in
+function glm() {
+    local count=${1:--1}
+    glogm -n $count
+}
+
+# ============================ Show ============================
+alias gbl="git blame --color-by-age --color-lines" # Show blame information with color-by-age and color-lines
+alias ggr="git grep --no-index -i -I --exclude-standard --heading --line-number" # Search for a string in the repository
+alias gsh="git show" # Show details of a commit
+alias gshno="git show --name-only" # Show names of files changed in a commit
+
+# ============================ Reset ============================
+alias grm="git reset --mixed" # Reset index but keep changes in the working directory (mixed mode)
+alias grhh="git reset HEAD --hard" # Discards all uncommitted changes (hard reset).
+
+# Reset the current branch to n commits before HEAD
+function grh() {
+  if [ -z "$1" ]; then
+    echo "Usage: grh <number-of-commits>"
+    return 1
+  fi
+  git reset HEAD~$1
+}
+
+# Reset the current branch to the specified commit and apply --hard
+function grch() {
+  if [ -z "$1" ]; then
+    echo "Usage: grch <commit-hash>"
+    return 1
+  fi
+  git reset "$1" --hard
+}
+
+# ============================ Diff ============================
+alias gd="git diff" # Show changes between commits, branches, or the working directory
+alias gds="git diff --staged" # Show changes in the staging area
 
 # Show the diff of a file based on a partial name match from modified files
 function gdgr() {
   __git_match_and_execute "gdgr" "$1" "git diff"
 }
 
+# ============================ Status ============================
+alias gs="git status" # Show the status of the working directory
+alias gss="git status -s" # Show a short status of the working directory
+
+# ============================ Reflog ============================
+alias gref="git reflog" # Show the reflog
+
+# ============================ File Checkout ============================
+# Check out a file based on a partial name match from modified files
+function gcofgr() {
+  __git_match_and_execute "gcofgr" "$1" "git checkout"
+}
+
+# ============================ Links ============================
 # Create a pull request and open it in the default browser
 function pr() {
   local github_url branch_name main_branch pr_url
@@ -181,6 +187,13 @@ function gh() {
   
   explorer.exe "$url"
 }
+
+# ============================ Utils ============================
+alias gdefault="git symbolic-ref refs/remotes/origin/HEAD | cut -d'/' -f4" # Get the default branch name
+alias gcurrent="git symbolic-ref --short HEAD" # Get the current branch name
+alias gcgl="git config --global --list" # List the current global Git configuration
+alias gcge="git config --global --edit" # Opens the global Git configuration file
+alias gcfd="git clean -fd" # Remove untracked files and directories
 
 # Execute a Git command on a file matched by partial name from the working directory
 # Usage: __git_match_and_execute <description> <partial-file-name> <git-command>
