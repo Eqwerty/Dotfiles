45302da62b84fcdb95ceabd6c3215093af61a7fd
Eduardo Quintana <eduardo.quintana@derivco.co.uk>
2025-10-23 12:24:21 +0200

    Improve cdm and catm

--- bashrc.sh
+++ bashrc.sh
@@ -14,34 +14,36 @@ function f() {
   ls -a | grep -i "$query"
 }
 
-# Change directory to the first match for a given string
+# Change directory to the first matching directory
 function cdm() {
-  local query="$1"
-  local results
-  IFS=$'\n' read -rd '' -a results < <(find . -maxdepth 1 -type d -iname "*$query*" -printf "%f\n")
-
-  if [[ ${#results[@]} -eq 0 ]]; then
-    echo -e "No directory found containing: $query"
-  elif [[ ${#results[@]} -eq 1 ]]; then
-    cd "${results[0]}" || echo "Failed to cd into ${results[0]}"
-  else
-    echo -e "\nMultiple directories found containing: $query\n"
-    printf -- "- %s\n" "${results[@]}"
-  fi
+  _match d "$1" cd "directory"
 }
 
-# Display the contents of a file if exactly one match is found
+# Display the contents of the first matching file
 function catm() {
-  local query="$1"
+  _match f "$1" cat "file"
+}
+
+function _match() {
+  local type="$1"
+  local query="$2"
+  local action="$3"
+  local noun="$4"
   local results
-  IFS=$'\n' read -rd '' -a results < <(find . -maxdepth 1 -type f -iname "*$query*" -printf "%f\n")
+
+  if [ -z "$query" ]; then
+    echo "Usage: ${action}m <partial-${noun}-name>"
+    return 1
+  fi
+
+  IFS=$'\n' read -rd '' -a results < <(find . -maxdepth 1 -type "$type" -iname "*$query*" -printf "%f\n")
 
   if [[ ${#results[@]} -eq 0 ]]; then
-    echo -e "No files found containing: $query"
+    echo -e "No ${noun}s found containing: $query"
   elif [[ ${#results[@]} -eq 1 ]]; then
-    cat "${results[0]}"
+    $action "${results[0]}" || echo "Failed to $action ${results[0]}"
   else
-    echo -e "\nMultiple files found containing: $query\n"
+    echo -e "\nMultiple ${noun}s found containing: $query\n"
     printf -- "- %s\n" "${results[@]}"
   fi
 }
